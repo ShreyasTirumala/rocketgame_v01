3eaf8b4c46907a096e688c4451101cffd894d9e4
Sarah Strohkorb <sarah.strohkorb@gmail.com>
2015-06-11 09:42:56 -0400

    Deleted old code (TouchAndDragPieces) and added the recycling objects code from the superzombierunner game for reference when we start to do object pooling and recycling game objects

---
+++ build_a_rocket_v01/Assets/Scripts/RecyclingGameObjects_SuperZombieRunner/RecycleGameObject.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// Interface being used so that we can look for other scripts that the recycle script
+// will need to reset whenever it gets reset itself (aka the Obstacle script)
+public interface IRecycle {
+
+	void Restart();
+	void ShutDown();
+
+}
+
+public class RecycleGameObject : MonoBehaviour {
+
+	private List<IRecycle> recycleComponents;
+
+	void Awake() {
+		var components = GetComponents<MonoBehaviour> ();
+		recycleComponents = new List<IRecycle> ();
+		foreach (var component in components) {
+			if (component is IRecycle) {
+				recycleComponents.Add (component as IRecycle);
+			}
+		}
+	}
+
+	public void Restart() {
+		gameObject.SetActive (true);
+
+		foreach (var component in recycleComponents) {
+			component.Restart();
+		}
+	}
+
+	// use instead of destroy call
+	public void ShutDown() {
+		gameObject.SetActive (false);
+		
+		foreach (var component in recycleComponents) {
+			component.ShutDown();
+		}
+	}
+
+}
